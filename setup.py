6ca3648a5079fc7e363e17d2c6c6d8abcdacc88d
ssbunyk <tbunyk@softserveinc.com>
2015-09-09 11:47:39 +0300

    change version to 1.0.3dev

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.WBEM"
-VERSION = "1.0.3"
+VERSION = "1.0.3dev"
 AUTHOR = "Zenoss"
 LICENSE = "All Rights Reserved"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
