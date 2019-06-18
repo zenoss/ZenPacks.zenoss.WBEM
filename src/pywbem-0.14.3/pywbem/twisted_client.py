50630dd663796934296eaa6cb0244a07b4d9c7d5
zenoss <zenoss@zendev2pac.zenoss.loc>
2019-06-18 16:05:04 -0500

    updated to support backward compatability

---
+++ src/pywbem-0.14.3/pywbem/twisted_client.py
@@ -0,0 +1,15 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2019, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+
+from pywbem import twisted_agent
+
+class ExecQuery(twisted_agent.ExecQuery):
+	# placeholder to maintain backward compatability
+	pass
