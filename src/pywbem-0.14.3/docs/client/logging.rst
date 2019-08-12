3682c9a39e0e54625f137cb557f19f54bb10059a
Jeff Powell <jpowell@zenoss.com>
2019-08-12 15:46:01 -0500

    do not include sub-lib

--- src/pywbem-0.14.3/docs/client/logging.rst
+++
@@ -1,9 +0,0 @@
-
-.. _`WBEM operation logging`:
-
-WBEM operation logging
-----------------------
-
-.. automodule:: pywbem._logging
-   :members:
-
--- src/pywbem-0.14.3/tools/python_bitsize.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import ctypes
-
-pointer_bits = ctypes.sizeof(ctypes.c_void_p) * 8
-
-print("Bit size of Python environment: {0} bit".format(pointer_bits))
